9fe448b6adb844bf85dc33370e20b8c17803ff8f
Trong_Minh <trongminhmcw10e@gmail.com>
2024-11-20 23:24:57 +0700

    update slide

--- labwork1/tcp-client.py
+++ labwork1/tcp-client.py
@@ -13,23 +13,31 @@ class Signal(Enum):
     REQUEST_A_REPO = b'RAR'
     DONE = b'D1'
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
-# Connecting with Server 
-sock.connect((host, port)) 
+
 
 filename = 'cli-send.txt'
-try: 
-# Reading file and sending data to server 
-    fi = open(filename, "r") 
-    data = fi.read() 
-    if not data: 
-        exit
-    while data: 
-        sock.send(str(data).encode()) 
-        data = fi.read() 
 
-    # File is closed after data is sent 
-    fi.close() 
+def send_signal(client_socket: socket.socket, signal:bytes ):
+    client_socket.send(signal)
+    pass
 
-except IOError: 
-    print('You entered an invalid filename!\nPlease enter a valid name') 
+def send_file():
+
+    pass
+
+def receive_file():
+
+    pass
+
+def send_repo():
+    
+    pass
+
+def receive_repo():
+
+    pass
+
+if __name__ == "__main__":
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
+    # Connecting with Server 
+    sock.connect((host, port)) 
