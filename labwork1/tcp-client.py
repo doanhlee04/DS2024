b949be129d08f9186e4bf1d4174a831a6ac819fe
Trong_Minh <trongminhmcw10e@gmail.com>
2024-11-23 01:53:23 +0700

    done with send repo from server to client

--- labwork1/tcp-client.py
+++ labwork1/tcp-client.py
@@ -68,12 +68,46 @@ def send_repo(client_socket : socket.socket, repo_name: str):
     client_socket.send(n_files)
 
     for file in files:
-        send_file(client_socket, file)
+        sleep(0.1)
+        client_socket.send(file.encode())
+        sleep(0.1)
+
+        with open(join(repo_name, file), 'rb') as f:
+            while chunk := f.read(BUFFERSIZE):
+                client_socket.send(chunk)
+
+        sleep(0.1)
+        send_signal(client_socket, Signal.DONE)
     
     pass
 
-def request_repo(client_socket : socket.socket, repo_req: str, repo_save: str):
+def request_repo(client_socket : socket.socket, repo_req: str, repo_save: str = r'client-receive/'):
     send_signal(client_socket, Signal.REQUEST_A_REPO )
+    sleep(0.1)
+    # Send repo name
+    client_socket.send(repo_req.encode())
+
+    # Get number of files in repo
+    n_files = client_socket.recv(4) # 4 bytes of an int
+    n_files = struct.unpack('!I', n_files)[0]
+    
+    print(f'number of files: {n_files}')
+    for _ in range(n_files):
+        file_name = client_socket.recv(BUFFERSIZE) # Get file name
+        file_name = file_name.decode()
+        save_file = join(repo_save, file_name)
+        print(f"Downloading file: {file_name}")
+
+        with open(save_file, 'wb') as file:
+            
+            while True:
+                data = client_socket.recv(BUFFERSIZE)
+
+                if not data or data == Signal.DONE.value:  # End signal
+                    break
+
+                file.write(data)
+
     pass
 
 if __name__ == "__main__":
@@ -82,9 +116,10 @@ if __name__ == "__main__":
     cli_sock.connect((HOST, PORT)) 
 
     # region do something
-    send_file(cli_sock, r'file-transfered/client-send/send0.txt')
+    # send_file(cli_sock, r'file-transfered/client-send/send0.txt')
     # request_file(cli_sock, 'ser-send.txt', 'recv0.txt')
     # send_repo(cli_sock, r'file-transfered/client-send/')
+    request_repo(cli_sock, 'file-transfered/server-send', r'file-transfered/client-receive/')
     sleep(0.1)
     send_signal(cli_sock, Signal.CLOSE_SERVER)
     # endregion
--- labwork1/tcp-server.py
+++ labwork1/tcp-server.py
@@ -11,13 +11,19 @@ def receive_signal(client_socket: socket.socket) -> bytes:
 def send_signal(client_socket: socket.socket, signal: Signal):
     client_socket.send(signal.value)
 
-def receive_file(client_socket: socket.socket, file_name: str = None):
+def receive_file(
+        client_socket: socket.socket, 
+        file_name: str | None = None, 
+        repo_to_save: str | None = None
+        ) -> None:
     """Receives a file from the client."""
 
     # Get file name from client side
     clif_name = client_socket.recv(BUFFERSIZE)
     clif_name = clif_name.decode()
+
     if file_name is None: file_name = clif_name
+    if repo_to_save is not None: file_name = os.path.join(repo_to_save, file_name)
 
     with open(file_name, 'wb') as file:
         print(f"Receiving file: {clif_name}")
@@ -30,7 +36,10 @@ def receive_file(client_socket: socket.socket, file_name: str = None):
 
     print(f"File received: {file_name}")
 
-def send_file(client_socket: socket.socket, file_name: str) -> None:
+def send_file(
+        client_socket: socket.socket, 
+        file_name: str
+        ) -> None:
     """Sends a file to the client."""
     if os.path.exists(file_name):
         print(f"Sending file: {file_name}")
@@ -56,9 +65,38 @@ def receive_repo(client_socket: socket.socket, save_folder: str = r'received-fil
     n_files = client_socket.recv(4) # 4 bytes of an int
     n_files = struct.unpack('!I', n_files)[0]
     
-    for i in range(n_files):
+    for _ in range(n_files):
+        receive_file(client_socket, repo_to_save=save_folder)
 
-        pass
+def send_repo(
+        client_socket: socket.socket, 
+        repo_name: str
+        ) -> None:
+    """Send a repository to client"""   
+
+    if os.path.exists(repo_name):
+
+        files = os.listdir(repo_name)
+        n_files = len(files)
+        n_files = struct.pack('!I', n_files)
+        client_socket.send(n_files)
+
+        for file in files:
+            sleep(0.1)
+            print(f"Sending file: {file}")
+            client_socket.send(file.encode()) # send file name
+
+            with open(os.path.join(repo_name, file), 'rb') as file:
+                while chunk := file.read(BUFFERSIZE):
+                    client_socket.send(chunk)
+
+            sleep(0.1)
+            send_signal(client_socket, Signal.DONE)  # End signal
+
+    else:
+        client_socket.send(b'ERROR: File not found')
+        print("File not found.")
+
     pass
 
 if __name__ == "__main__":
@@ -81,11 +119,15 @@ if __name__ == "__main__":
                 receive_file(client_socket, file_name=r'file-transfered/server-receive/recv0.txt')
 
             if sig == Signal.REQUEST_A_FILE.value:
-                file_name = receive_signal(client_socket)
+                file_name = client_socket.recv(BUFFERSIZE)
                 send_file(client_socket, file_name.decode())
 
             if sig == Signal.SEND_A_REPO.value:
                 receive_repo(client_socket, r'file-transfered/server-receive/')
+
+            if sig == Signal.REQUEST_A_REPO.value:
+                repo_name = client_socket.recv(BUFFERSIZE)
+                send_repo(client_socket, repo_name.decode())
 
             if not sig:
                 break
